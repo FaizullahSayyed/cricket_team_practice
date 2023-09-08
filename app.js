b9117d7309baace85fe501deb1db1d100e17ddd5
FaizullahSayyed <faizs7412@gmail.com>
2023-09-08 13:48:29 +0000

    test cases failed

--- app.js
+++ app.js
@@ -29,13 +29,51 @@ const initializeDBAndServer = async () => {
 
 initializeDBAndServer();
 
+// const convertDBObjectToResponseObject = (DBObject) => {
+//   return {
+//     playerId: DBObject.player_id,
+//     playerName: DBObject.player_name,
+//     jerseyNumber: DBObject.jersey_number,
+//     role: DBObject.role,
+//   };
+// };
+
+// app.get("/players/", async (request, response) => {
+//   const getAllPlayerQuery = `
+//     SELECT *
+//     FROM cricket_team;
+//     `;
+//   const playersArray = await db.all(getAllPlayerQuery);
+//   //   response.send(
+//   //     playersArray.map((eachPlayer) => {
+//   //       convertDBObjectToResponseObject(eachPlayer);
+//   //       console.log(eachPlayer);
+//   //     })
+//   //   );
+//   response.send(playersArray);
+// });
+
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+//Get Player API
 app.get("/players/", async (request, response) => {
   const getAllPlayerQuery = `
     SELECT *
     FROM cricket_team;
     `;
   const playersArray = await db.all(getAllPlayerQuery);
-  response.send(playersArray);
+  response.send(
+    playersArray.map(
+      (eachPlayer) => convertDbObjectToResponseObject(eachPlayer) // update here
+    )
+  );
 });
 
 app.post("/players/", async (request, response) => {
@@ -44,16 +82,16 @@ app.post("/players/", async (request, response) => {
   const { player_id, player_name, jersey_number, role } = playerDetails;
 
   const addPlayerQuery = `
-INSERT INTO cricket_team
-(player_id, player_name, jersey_number, role)
-VALUES
-(
-    ${player_id},
-    ${player_name},
-    ${jersey_number},
-    ${role}
-);
-`;
+    INSERT INTO cricket_team
+    (player_id, player_name, jersey_number, role)
+    VALUES
+    (
+        ${player_id},
+        '${player_name}',
+        ${jersey_number},
+        '${role}'
+    );
+    `;
   const dbResponse = await db.run(addPlayerQuery);
   //   const playerId = dbResponse.lastID;
   response.send("Player Added to Team");
@@ -68,22 +106,20 @@ app.get("/players/:playerId/", async (request, response) => {
   `;
   const playerDetails = await db.get(getPlayerQuery);
   response.send(playerDetails);
-  console.log("Player Added to Team");
 });
 
 app.put("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const playerDetails = request.body;
 
-  const { playerId_, PlayerName, jerseyNumber, role } = playerDetails;
+  const { playerId_, player_name, jerseyNumber, role } = playerDetails;
 
   const updatePlayerDetailsQuery = `
     UPDATE TABLE cricket_team
     SET
-    player_id = ${playerId_},
-    player_name = ${playerName},
+    player_name = '${playerName}',
     jersey_number = ${jerseyNumber},
-    role = ${role}
+    role = '${role}'
     WHERE player_id = ${playerId};
     `;
   await db.run(updatePlayerDetailsQuery);
@@ -93,9 +129,9 @@ app.put("/players/:playerId/", async (request, response) => {
 app.delete("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const deletePlayerQuery = `
-DELETE FROM cricket_team
-WHERE player_id = ${playerId};
-`;
+    DELETE FROM cricket_team
+    WHERE player_id = ${playerId};
+    `;
   await db.run(deletePlayerQuery);
   response.send("Player Removed");
 });
